a8d38e9a46a793fd41e89add7f028fac2debcabf
Maxim Shipachev <maxim@shipachev.ru>
2026-03-23 19:20:40 +0700

    Редактирование сессий

--- sw.js
+++ sw.js
@@ -1,20 +1,26 @@
-const CACHE = 'balance-v1';
+const CACHE = 'balance-v2.0';
 const ASSETS = ['/', '/index.html'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches
+            .keys()
+            .then((keys) =>
+                Promise.all(
+                    keys
+                        .filter((k) => k !== CACHE)
+                        .map((k) => caches.delete(k)),
+                ),
+            ),
+    );
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
